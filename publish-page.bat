2d3941871de0dded970a73c483992cc4bcf3b92f
OtwakO <tom28055101@gmail.com>
2024-12-01 05:38:48 +0800

    ..

--- publish-page.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@REM git subtree push --prefix github-page origin gh-pages
-@REM First create worktree with git worktree add github-page gh-pages (Remember using admin powershell would cause problem)
-cd github-page
-git add *
-git commit -m "Deploy updated GitHub Pages"
-git push -uf origin gh-pages
-pause
